455e596adf777fb88b7a476cb7241d813f0e5ddc
Marcio Bittencourt <marciobitt97@gmail.com>
2021-09-11 23:22:17 -0300

    search field development

--- src/components/appo/goal/index.tsx
+++ src/components/appo/goal/index.tsx
@@ -1,6 +1,7 @@
 import Style from "./goal.module.scss";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import Smart from './smart/index';
+import { Search } from "react-bootstrap-icons";
 
 type Props = {};
 
@@ -19,7 +20,7 @@ const Goal = (props: Props) => {
         <Row className={Style.goalSection}>
             <Col>
                 <Row className={Style.goalSectionHeader}>
-                    <Col className={Style.goalTitle} lg={9}>
+                    <Col className={Style.goalTitle} lg={8}>
                         <input type="text" placeholder="Insira o titulo do objetivo aqui!" />
                     </Col>
                     <Col lg={3}>
@@ -30,10 +31,42 @@ const Goal = (props: Props) => {
                     <Col lg={8}>
                         <textarea placeholder="Descreve de forma detalhada seu objetivo..." id="description"></textarea>
                     </Col>
+                    <Col lg={2}>
+                        <label>Inicio:</label>
+                        <input type="date" className={Style.goalDate} />
+                        <select defaultValue="none" className={`${Style.sectorInput} form-control`}>
+                            <option value="none" disabled> - Selecione o setor - </option>
+                            <option value="1"> - Selecione - </option>
+                            <option value="2"> - Selecione - </option>
+                            <option value="3"> - Selecione - </option>
+                            <option value="4"> - Selecione - </option>
+                        </select>
+                        <div className={Style.searchEmployee}>
+                            <input
+                                list="employees"
+                                className="form-control"
+                                type="text"
+                                placeholder="Search"
+                                aria-label="Search" />
+                            <Search className={Style.icon}/>
+                            <datalist id="employees">
+                                <option value="Frodo Bolseiro" />
+                                <option value="Gandalf Cinzento" />
+                                <option value="Aragorn" />
+                                <option value="Legolas" />
+                                <option value="Marcus Bittencourt" />
+                                <option value="Marcio Bittencourt" />
+                            </datalist>
+                        </div>
+                    </Col>
+                    <Col lg={2}>
+                        <label>Fim:</label>
+                        <input type="date" />
+                    </Col>
                 </Row>
                 <Row className={Style.goalSectionContent} lg={8}>
                     <Col lg={8}>
-                        <textarea placeholder="Descreve de forma detalhada seu objetivo..." id="description"></textarea>
+                        <textarea placeholder="Descreve de como o objetivo será mensurado..." id="description"></textarea>
                     </Col>
                 </Row>
             </Col>
