80fe76128f5523eb85dbae58a92dbface1a1635d
Marcio Bittencourt <marciobitt97@gmail.com>
2021-11-06 21:25:25 -0300

    update route DISC

--- src/pages/disc.page.tsx
+++ src/pages/disc.page.tsx
@@ -1,23 +1,23 @@
 import { Col, Container, Row } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 import DISCData from '../assets/disc.json';
 import Survey from '../components/disc/survey';
 import DashboardDisc from '../components/disc/dashboard';
 import Style from './disc.page.module.scss';
+import { ChevronCompactLeft } from 'react-bootstrap-icons';
 
 const DISCPage = () => {
     return (
         <Container className={Style.page}>
             <Row>
-                <Col>
+                <Col className={Style.pageHeader}>
                     <h3 className={Style.pageTitle} >DISC</h3>
+                    <Link className={Style.btnPrimary} to={`/app/disc`}>
+                        <ChevronCompactLeft className={Style.backIcon} />
+                        Voltar
+                    </Link>
                 </Col>
             </Row>
-            {/* <Row className={Style.content}>
-                <Col lg={6} className={Style.webform}>
-                    <Survey askings={DISCData.askings} />
-                </Col>
-            </Row>  */}
             <DashboardDisc />
         </Container>
     )
